96b1e153faa9a14ced6a987038f32ec35feb9cdd
Aaron <devilfruits1999@gmail.com>
2019-10-15 16:20:02 -0400

    Lab 3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,7 +6,51 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string runAgain = "y";
+
+            while (runAgain != "n")
+            {
+                Console.Write("Please enter a number between 1 and 100: ");
+                int userInput = int.Parse(Console.ReadLine());
+
+                //if userInput is even
+                if (userInput >= 1 && userInput <= 100)
+                {
+                    if (userInput % 2 == 0)
+                    {
+                        if (userInput >= 2 && userInput <= 25)
+                        {
+                            Console.WriteLine("Even and less than 25.");
+                        }
+                        if (userInput >= 26 && userInput <= 60)
+                        {
+                            Console.WriteLine("Even");
+                        }
+                        if (userInput > 60)
+                        {
+                            Console.WriteLine($"{userInput} and even");
+                        }
+                    }
+                    else
+                    {
+                        if (userInput > 60)
+                        {
+                            Console.WriteLine("odd and greater than 60");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{userInput}, Odd.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input was not between 1 and 100");
+                }
+
+                Console.WriteLine("Run Again?(y/n)");
+                runAgain = Console.ReadLine();
+            } 
         }
     }
 }
